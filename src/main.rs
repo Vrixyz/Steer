c6ec84ccddd6806a4d34007f72bbf7a5784c174a
Thierry Berger <contact@thierryberger.com>
2021-04-23 23:41:26 +0200

    spawn bullets

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,22 @@ pub struct MainCamera;
 pub struct PlayerCommander;
 pub struct PlayerCommanderTargetLine;
 
+#[derive(PartialEq)]
+pub enum FireMode {
+    Active,
+    Idle,
+}
+
+// TODO: we can split input for movement and input for attack
 pub struct CommanderInput {
-    pub target_pos: Vec2,
+    pub desired_direction: Vec2,
+    pub fire_mode: FireMode,
+    pub fire_target: Vec2,
+}
+
+pub struct AttackAbility {
+    pub cooldown: f32,
+    pub last_attack: f32,
 }
 
 pub struct Velocity(Vec2);
@@ -29,29 +43,15 @@ pub struct SteeringManager {
     : Vec2,
 }
 
-const MAX_SPEED: f32 = 100.0;
-
-/// From https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Math/Vector3.cs
-fn move_towards(current: Vec2, target: Vec2, maxDistanceDelta: f32) -> Vec2 {
-    // avoid vector ops because current scripting backends are terrible at inlining
-    let to_vector_x = target.x - current.x;
-    let to_vector_y = target.y - current.y;
-
-    let sqdist = to_vector_x * to_vector_x + to_vector_y * to_vector_y;
-
-    if sqdist == 0.0 || (maxDistanceDelta >= 0.0 && sqdist <= maxDistanceDelta * maxDistanceDelta) {
-        return target;
-    }
-    let dist = sqdist.sqrt();
-
-    return Vec2::new(
-        current.x + to_vector_x / dist * maxDistanceDelta,
-        current.y + to_vector_y / dist * maxDistanceDelta,
-    );
+pub struct MyAssets {
+    pub bullet: Handle<ColorMaterial>,
+    pub ally: Handle<ColorMaterial>,
 }
 
+const MAX_SPEED: f32 = 100.0;
+
 impl SteeringManager {
-    pub fn do_seek(current_position: Vec2, target: Vec2, current_speed: Vec2) -> Vec2 {
+    pub fn do_seek(current_position: Vec2, target: Vec2, current_speed: Vec2, mass: f32) -> Vec2 {
         let mut desired = target - current_position;
         let distance = desired.length();
 
@@ -63,15 +63,19 @@ impl SteeringManager {
         } else {
             desired *= MAX_SPEED;
         }
+        return Self::do_desired(desired, current_speed, mass);
+    }
+    pub fn do_desired(desired: Vec2, current_speed: Vec2, mass: f32) -> Vec2 {
         let force = desired - current_speed;
 
-        return force;
+        return force / mass;
     }
 }
 
 ///This example is for performance testing purposes.
 ///See https://github.com/bevyengine/bevy/pull/1492
 fn main() {
+    
     App::build()
         //.add_plugin(LogDiagnosticsPlugin::default())
         //.add_plugin(FrameTimeDiagnosticsPlugin::default())
@@ -80,7 +84,9 @@ fn main() {
             frustum_culling_enabled: true,
         })
         .insert_resource(CommanderInput {
-            target_pos: Vec2::splat(0.0),
+            desired_direction: Vec2::splat(0.0),
+            fire_mode: FireMode::Idle,
+            fire_target: Vec2::splat(0.0),
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(DebugLinesPlugin)
@@ -90,6 +96,7 @@ fn main() {
         .add_startup_system(setup.system())
         //.add_system(tick.system().label("Tick"))
         .add_system(my_input_system.system())
+        .add_system(commander_attack_apply.system())
         .add_system(steering_targets_influence.system().label("steering_update"))
         .add_system(commander_input_apply.system().before("steering_update"))
         .add_system(velocity.system().after("steering_update"))
@@ -106,6 +113,12 @@ fn setup(
 
     let tile_size = Vec2::splat(64.0);
     let sprite_handle = materials.add(assets.load("branding/icon.png").into());
+
+    commands
+    .insert_resource(MyAssets {
+        bullet: sprite_handle.clone(),
+        ally: sprite_handle.clone(),
+    });
 
     commands
         .spawn()
@@ -127,6 +140,10 @@ fn setup(
         .insert(SteeringManager {
             steering_target
             : Vec2::default(),
+        })
+        .insert(AttackAbility {
+            cooldown: 1.0/2.0,
+            last_attack: 0.0,
         });
 }
 
@@ -141,21 +158,71 @@ fn commander_input_apply(
 ) {
     for (transform, mut commander, velocity, _) in steering_managers.iter_mut() {
         commander.steering_target
+         = SteeringManager::do_desired(
+            commander_input.desired_direction * MAX_SPEED,
+            velocity.0,
+            20.0
+        );
+    }
+}
+fn commander_attack_apply(
+    mut commands: Commands,
+    time: Res<Time>,
+    assets: Res<MyAssets>,
+    commander_input: ResMut<CommanderInput>,
+    mut attacker: Query<(
+        &Transform,
+        &mut AttackAbility,
+    )>,
+) {
+    for (transform, mut attack) in attacker.iter_mut() {
+        if commander_input.fire_mode == FireMode::Active {
+            let time_since_startup = time.time_since_startup().as_secs_f32();
+            if attack.last_attack + attack.cooldown < time_since_startup {
+                attack.last_attack = time_since_startup;
+                let position = transform.translation;
+                commands
+                .spawn()
+                .insert_bundle(SpriteBundle {
+                    material: assets.bullet.clone(),
+                    sprite: Sprite::new(Vec2::splat(16.0)),
+                    transform: Transform::from_translation(position),
+                    ..Default::default()
+                })
+                .insert(Velocity((commander_input.fire_target - position.into()).normalize_or_zero() * MAX_SPEED * 2.0));
+            
+            }
+        }
+    }
+}
+/*fn commander_input_apply(
+    commander_input: ResMut<CommanderInput>,
+    mut steering_managers: Query<(
+        &Transform,
+        &mut SteeringManager,
+        &Velocity,
+        &PlayerCommander,
+    )>,
+) {
+    for (transform, mut commander, velocity, _) in steering_managers.iter_mut() {
+        commander.steering_target
          = SteeringManager::do_seek(
             transform.translation.into(),
             commander_input.target_pos,
             velocity.0,
+            20.0
         );
     }
-}
+}*/
 
 fn steering_targets_influence(
+    time: Res<Time>, 
     mut steering_managers: Query<(&Transform, &mut Velocity, &mut SteeringManager)>,
 ) {
     for (_transform, mut velocity, mut manager) in steering_managers.iter_mut() {
         manager.steering_target
          = manager.steering_target
-        .clamp_length_max(10.0);
+        .clamp_length_max(100.0);
 
         velocity.0 += manager.steering_target
         ;
@@ -187,33 +254,51 @@ fn move_camera(
 }
 
 pub fn my_input_system(
-    window: ResMut<Windows>,
+    window: Res<Windows>,
+    mut commander_input: ResMut<CommanderInput>,
+    mouse_button_input: Res<Input<MouseButton>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut ev_cursor: EventReader<CursorMoved>,
     // query to get camera transform
     q_camera: Query<&Transform, With<MainCamera>>,
-    mut ev_cursor: EventReader<CursorMoved>,
-    mut ev_mousebtn: EventReader<MouseButtonInput>,
-    mut commander_input: ResMut<CommanderInput>,
 ) {
-    for ev in ev_mousebtn.iter() {
-        if ev.state.is_pressed() {
-            if let Some(pos) = ev_cursor.iter().last() {
-                let camera_transform = q_camera.iter().next().unwrap();
-                let wnd = window.get(pos.id).unwrap();
-                let size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+    if mouse_button_input.pressed(MouseButton::Left) {
+        commander_input.fire_mode = FireMode::Active;
+    }
+    else {
+        commander_input.fire_mode = FireMode::Idle;
+    }
+    if (keyboard_input.is_changed()) {
 
-                // the default orthographic projection is in pixels from the center;
-                // just undo the translation
-                let p = pos.position - size / 2.0;
-
-                // apply the camera transform
-                let pos_wld = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0);
-                commander_input.target_pos = pos_wld.into();
-                eprintln!(
-                    "Just pressed mouse button: {:?} at {:?}",
-                    ev.button, pos.position
-                );
-            }
+        let mut movement = Vec2::splat(0.0);
+        if keyboard_input.pressed(KeyCode::Z) {
+            movement += Vec2::unit_y();
         }
+        if keyboard_input.pressed(KeyCode::S) {
+            movement += -Vec2::unit_y();
+        }
+        if keyboard_input.pressed(KeyCode::D) {
+            movement += Vec2::unit_x();
+        }
+        if keyboard_input.pressed(KeyCode::Q) {
+            movement += -Vec2::unit_x();
+        }
+        commander_input.desired_direction = movement.normalize_or_zero();
+    }
+
+    if let Some(pos) = ev_cursor.iter().last() {
+        let camera_transform = q_camera.iter().next().unwrap();
+        let wnd = window.get(pos.id).unwrap();
+        let size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+
+        // the default orthographic projection is in pixels from the center;
+        // just undo the translation
+        let p = pos.position - size / 2.0;
+
+        // apply the camera transform
+        let pos_wld = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0);
+
+        commander_input.fire_target = pos_wld.into();
     }
 }
 pub fn command_debug(
@@ -225,7 +310,7 @@ pub fn command_debug(
         Some(it) => it,
         _ => return,
     };
-    lines.line(c.1.translation, commander_input.target_pos.extend(0.0), 0.0);
+    lines.line(c.1.translation, c.1.translation + commander_input.desired_direction.extend(0.0) * MAX_SPEED, 0.0);
 }
 
 pub fn steering_debug(
